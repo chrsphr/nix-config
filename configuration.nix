6444ed52ccba46962a9b16bcd3bd9e058f320c60
Chris McNeill <chris@mcneill.fyi>
2025-12-17 18:36:41 +0000

    adding a few things like teams, better fonts. Still don't have hibernate working

--- configuration.nix
+++ configuration.nix
@@ -106,21 +106,45 @@
     description = "Chris";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-	beeper
-	qgis
-	git
-	vscode
-	spotify
-	conda
-	fastfetch
-  powertop
-	fprintd
-#	teams
+    beeper
+    qgis
+    git
+    vscode
+    spotify
+    conda
+    fastfetch
+    powertop
+    dig
+	  fprintd
+	  teams-for-linux
+    google-fonts
     ];
   };
 
   services.fprintd.enable = true;
 
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
+  };
 
   # Install firefox.
   programs.firefox.enable = true;
@@ -180,3 +204,4 @@
   system.stateVersion = "25.11"; # Did you read the comment?
 
 }
+

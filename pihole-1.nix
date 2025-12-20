d1cbb943de0af071c2a7e0f633f1239b532c1a4f
Chris McNeill <chris@mcneill.fyi>
2025-12-20 22:43:51 +0000

    trying to set the hostname

--- pihole-1.nix
+++ pihole-1.nix
@@ -6,8 +6,8 @@
     ./pihole-common.nix
   ];
 
-services.pihole-ftl.settings.webserver.domain = "pihole-1.mcneill.fyi";
-  
+  services.pihole-web.hostName = lib.mkForce "pihole-1.mcneill.fyi";
+
   networking.interfaces.eth0.ipv4.addresses = lib.mkForce [{
     address = "192.168.1.9";
     prefixLength = 24;

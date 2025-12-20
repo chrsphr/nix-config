24fffb406ba28937bc58e1b3f893ed5b31535f9d
Chris McNeill <chris@mcneill.fyi>
2025-12-20 15:16:09 +0000

    test

--- pihole-1.nix
+++ pihole-1.nix
@@ -1,7 +1,15 @@
-{ ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [
-    (import ./pihole-common.nix { hostname = "pihole-1"; ip = "192.168.1.9"; })
+    ./common.nix
+    ./pihole-common.nix
   ];
+
+  networking.hostName = "pihole-1";
+  
+  networking.interfaces.eth0.ipv4.addresses = lib.mkForce [{
+    address = "192.168.1.9";
+    prefixLength = 24;
+  }];
 }

df39cee4ddfdd21e978112cff23e4a9f46d40823
Chris McNeill <chris@mcneill.fyi>
2026-05-21 19:47:20 +0100

    reverting wake changes

--- chris-desktop.nix
+++ chris-desktop.nix
@@ -99,8 +99,7 @@
     script = ''
       echo GPP0 > /proc/acpi/wakeup
       echo GPP8 > /proc/acpi/wakeup
-      echo PTXH > /proc/acpi/wakeup
-      echo XHC0 > /proc/acpi/wakeup
+
     '';
     serviceConfig = {
       Type = "oneshot";

8a7cf3869967305dd02c73b2da18bebe58fda3a1
Chris <chris@mcneill.fyi>
2026-05-30 14:45:24 +0000

    regen framework hardware config post-disko

--- hardware/desktop.nix
+++ hardware/desktop.nix
@@ -9,15 +9,10 @@
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
-
-
-
-
-
-
-
